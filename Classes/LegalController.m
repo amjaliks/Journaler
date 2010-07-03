6773a2d016003eb6f0ae38d888c8d6f4ca749bd5
amjaliks <amjaliks@4ce4c653-be4f-4523-8f62-2c479f5a16e7>
2010-07-03 15:28:58 +0000

    LJ-101

--- Classes/LegalController.m
+++ Classes/LegalController.m
@@ -13,17 +13,6 @@
 
 @synthesize webView;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -38,20 +27,6 @@
 	return interfaceOrientation == UIDeviceOrientationPortrait || UIDeviceOrientationIsLandscape(interfaceOrientation);
 }
 #endif
-
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 
 - (void)dealloc {
     [super dealloc];
